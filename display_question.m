d3c5a62fc714c0f80f212d79d573395326657724
gufett0 <104091627+gufett0@users.noreply.github.com>
2022-05-05 20:04:47 +0200

    Update display_question.m

--- display_question.m
+++ display_question.m
@@ -1,1 +1,13 @@
-function [] = display_question(w,col,instruct, txtloc, fontSize)% [] = display_question(w,col,instruct,txtloc)%% W = the window you pass into SCREEN%% COL = color, e.g. WHITE_INDEX%% INSTRUCT = 1 for 'longer' and 2 for 'shorter'%% TXTLOC = x and y location of the text% if you change these questionsif instruct == 1		Screen(w,'TextSize',fontSize);	txt = 'Quale lato è il più LUNGO?';elseif instruct == 2	Screen(w,'TextSize',fontSize);	txt = 'Quale lato è il più CORTO?';	        elseif  strcmp(instruct, 'UD')    Screen(w,'TextSize',fontSize);    txt = 'Premi il tasto: SX se punta in ALTO, DX se punta in BASSO';             elseif  strcmp(instruct, 'DU')    Screen(w,'TextSize',fontSize);    txt = 'Premi il tasto: SX se punta in BASSO, DX se punta in ALTO';         end[x y] = Screen(w, 'DrawText', txt, txtloc(1), txtloc(2), col);
+function [] = display_question(w, col, instruct, txtloc, fontSize)
+
+% w = the window you pass into SCREEN; col = color, e.g. WHITE_INDEX, instruct = 1 for 'longer' and 2 for 'shorter', txtloc = x and y location of the text
+
+if instruct == 1	
+	Screen(w,'TextSize',fontSize);
+	txt = 'Which side is LONGER?';
+elseif instruct == 2
+	Screen(w,'TextSize',fontSize);
+	txt = 'Which side is SHORTER?';	     
+end
+
+[x y] = Screen(w, 'DrawText', txt, txtloc(1), txtloc(2), col);
