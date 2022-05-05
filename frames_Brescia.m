401dff83fa01c904a1d9d93283bd4f70c6c1284a
gufett0 <104091627+gufett0@users.noreply.github.com>
2022-05-05 19:56:46 +0200

    Update frames_Brescia.m

--- frames_Brescia.m
+++ frames_Brescia.m
@@ -1,11 +1,7 @@
 function [actual_frames, actual_seconds, nominal_frames, nominal_seconds] = frames_Brescia(secs, ifi)
 
-% first output = numer of frames output already shortened by a 0.5*frames to insert into
-% the (Screen('Flip'), VBL)  as waiting
-
+% first output = numer of frames output already shortened by a 0.5*frames to insert into the (Screen('Flip'), VBL)  as waiting
 % second output = true seconds to wait for the next flip to happen
-
-
 
 % rounds the indicated time to an exact number of frames
 nominal_frames = round(secs/ifi);
@@ -15,9 +11,6 @@ actual_frames =  nominal_frames - 0.5;
 
 % true waiting
 actual_seconds = actual_frames*ifi;
-
-
 nominal_seconds = secs;
-
 
 end
