468c54e45457019931b1036f61c28149ff55c0cf
gufett0 <104091627+gufett0@users.noreply.github.com>
2022-05-06 12:18:46 +0200

    Update draw_pnline.m

--- draw_pnline.m
+++ draw_pnline.m
@@ -1,12 +1,13 @@
-function [] = draw_pnline(w,center,line_w, line_rightshift,col, penThickness, line_h)
+function [] = draw_pnline(w,center,line_w, line_rightshift, col, penThickness, line_h)
 
 % draw a horizontal line of length LINE_W, 
 % shifted upwards by LINE_UPSHIFT and 
 % shifted rightwards by LINE_RIGHTSHIFT
-% penThickness = 2;%thickness of the line
-% line_w = 500;%width in the number of pixels
+% penThickness is thickness of the line
+% line_w is width in the number of pixels
 % line_rightshift = 0;
-    line_upshift = 0;
+line_upshift = 0;
+
 %draw line parameters
 fromH1 = center(1) - line_w/2 + line_rightshift; %beginning x of line
 fromV1 = center(2) + line_upshift; %beginning y of line
